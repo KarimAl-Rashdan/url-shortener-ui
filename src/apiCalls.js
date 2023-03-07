ec88cbba4d1a6463507e729885a567645954abe0
karimy <karim.alrashdan14@gmail.com>
2023-03-07 09:37:11 -0700

    Update apiCalls getUrls function

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,10 @@
 export const getUrls = () => {
   return fetch('http://localhost:3001/api/v1/urls')
-      .then(response => response.json())
+      .then((response) => {
+        if(!response.ok) {
+          throw new Error("Something went wrong. Try again later!")
+        } else {
+          return response.json()
+        }
+      })
 }
