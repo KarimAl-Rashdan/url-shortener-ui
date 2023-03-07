fcc8b0a98e9701a3fe520f77edb5716c2b810565
karimy <karim.alrashdan14@gmail.com>
2023-03-07 09:59:40 -0700

    Complete iteration 1

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { getUrls } from '../../apiCalls';
 import UrlContainer from '../UrlContainer/UrlContainer';
 import UrlForm from '../UrlForm/UrlForm';
 
-export class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      urls: [],
-      error:""
-    }
-  }
+function App () {
+  const [urls, setUrls] = useState([])
+  const [error, setError] = useState("")
 
-  componentDidMount() {
-    getUrls()
-    .then(data => this.setState({urls : data}))
-    .catch(error => this.setState({error: error.message}))
-  }
+  useEffect(() => {
+    (async () => {
+   await getUrls()
+    .then(data => setUrls(data.urls))
+    .catch(error => setError(error.message))
+    })()
+  }, [])
 
-  render() {
     return (
       <main className="App">
         <header>
@@ -27,10 +23,10 @@ export class App extends Component {
           <UrlForm />
         </header>
 
-        <UrlContainer urls={this.state.urls}/>
+        <UrlContainer urls={urls}/>
       </main>
     );
-  }
+  
 }
 
 export default App;
